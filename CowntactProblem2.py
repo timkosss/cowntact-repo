7e408d9bdebd20358a000d8f74f4181e8574e175
Tim K #2 <507246@abington.k12.pa.us>
2024-05-21 14:23:03 -0400

    Im so cooked

--- CowntactProblem2.py
+++ CowntactProblem2.py
@@ -18,21 +18,41 @@ class CowFarm:
             elif temp == 0:
                 pass
             else:
+                if edge:
+                    temp = (temp * 2) - 1
+                    edge = False
                 if first or temp < smallest:
                     smallest = temp
                     first = False
                 temp = 0
+            i += 1
         return smallest
     
-    def maxDays(numCows):
-        return int(numCows / 2)
+    def maxDays(self):
+        return int(self.findSmallestLine() / 2)
     
-    def numOrigin(numCows, numDays):
+    def numOrigin(self, numCows, numDays):
         magicNum = (numDays * 2) + 1
         return math.ceil(numCows/magicNum)
+    
+    def findTheInfected(self):
+        total = 0
+        tempCluster = 0
+        days = self.maxDays()
+        for i in self.cowList:
+            if i == 1:
+                tempCluster += 1
+            elif tempCluster == 0:
+                pass
+            else:
+                total += self.numOrigin(tempCluster, days)
+                tempCluster = 0
+        return total
+
         
 
 
 
-cowNum = "111110111110"
+cowNum = "11111"
 cowLine = CowFarm(cowNum)
+print(cowLine.findTheInfected())
