204f37fe92192ce963110f946cb18cd23ed7ed01
Timka <timothy.kossenkov@gmail.com>
2024-05-20 22:51:29 -0400

    Fixing Up

--- CowntactProblem2.py
+++ CowntactProblem2.py
@@ -1,2 +1,35 @@
+import math
 class CowFarm:
-    x = 2
+    def __init__(self, cows) :
+        self.cowList = []
+        for i in cowNum:
+            self.cowList.append(int(i))
+    
+    def findSmallestLine(self):
+        first = True
+        temp = 0
+        i = 0
+        while i < len(self.cowListcowList):
+            if self.cowList[i] == 1:
+                temp+=1
+            elif temp == 0:
+                pass
+            else:
+                if first or temp < smallest:
+                    smallest = temp
+                    first = False
+                temp = 0
+        return smallest
+    
+    def maxDays(numCows):
+        return int(numCows / 2)
+    
+    def numOrigin(numCows, numDays):
+        magicNum = (numDays * 2) + 1
+        return math.ceil(numCows/magicNum)
+        
+
+
+
+cowNum = "111110111110"
+cowLine = CowFarm(cowNum)
